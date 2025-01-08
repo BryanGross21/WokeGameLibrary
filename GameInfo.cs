113aff159f805505eb42ec435c7144780a438277
Bryan Gross <111697373+BryanGross21@users.noreply.github.com>
2025-01-07 19:42:02 -0600

    Add project files.

---
+++ GameInfo.cs
@@ -0,0 +1,97 @@
+﻿using System;
+
+namespace WokeGameLibrary
+{
+	public enum Recommendation 
+	{
+		WOKE,
+		NOT_WOKE,
+		SOMEWHAT_WOKE
+	}
+	public class GameInfo
+	{
+		private string _name = "";
+
+		public string Name 
+		{
+			get 
+			{
+				return _name;
+			}
+			set 
+			{
+				_name = value;
+			}
+		}
+
+		private string _developer = "";
+
+		public string Developer 
+		{
+			get 
+			{
+				return _developer;
+			}
+			set 
+			{
+				_developer = value;
+			}
+		}
+
+		private string _publisher = "";
+
+		public string Publisher 
+		{
+			get 
+			{
+				return _publisher;
+			}
+			set 
+			{
+				_publisher = value;
+			}
+		}
+
+		private string _description = "";
+
+		public string Description 
+		{
+			get 
+			{
+				return _description;
+			}
+			set 
+			{
+				_description = value;
+			}
+		}
+
+		private string _releaseYear = "";
+
+		public string releaseYear 
+		{
+			get 
+			{
+				return _releaseYear;
+			}
+			set 
+			{
+				_releaseYear = value;
+			}
+		}
+
+		private Recommendation _recommendation;
+
+		public Recommendation isWoke 
+		{
+			get 
+			{
+				return _recommendation;
+			}
+			set 
+			{
+				_recommendation = value;
+			}
+		}
+	}
+}
---
+++ GameList.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WokeGameLibrary
+{
+	public class GameList
+	{
+		public ArrayList gameList = new();
+
+		string FileName = Environment.CurrentDirectory + "\\wokegames.txt";
+
+		public GameList() 
+		{
+			string[] gameInfo;
+			using (StreamReader sr = new(FileName)) 
+			{
+				string currentLine;
+				while ((currentLine = sr.ReadLine()) != null) 
+				{
+					gameInfo = currentLine.Split("}");
+					GameInfo newGame = new();
+					newGame.Name = gameInfo[0];
+					newGame.releaseYear = gameInfo[1];
+					newGame.Developer = gameInfo[2];
+					newGame.Publisher = gameInfo[3];
+					if (gameInfo[4] == "Recommended")
+					{
+						newGame.isWoke = Recommendation.NOT_WOKE;
+					}
+					else if (gameInfo[4] == "Informational")
+					{
+						newGame.isWoke = Recommendation.SOMEWHAT_WOKE;
+					}
+					else 
+					{
+						newGame.isWoke = Recommendation.WOKE;
+					}
+					newGame.Description = gameInfo[5];
+					gameList.Add(newGame);
+				}
+			}
+		}
+	}
+}
